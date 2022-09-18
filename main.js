50b6360833ff25288adb5a7c143f059b1d26b6de
JorgeCoM <rox.lenox@gmail.com>
2022-09-18 02:25:22 -0400

    update

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 // Cambio de imagenes desde botones
 let imgArray = [
-    '../images/image-product-1.jpg',
-    '../images/image-product-2.jpg',
-    '../images/image-product-3.jpg',
-    '../images/image-product-4.jpg',
+    './images/image-product-1.jpg',
+    './images/image-product-2.jpg',
+    './images/image-product-3.jpg',
+    './images/image-product-4.jpg',
 ]
 
 let nextBtn = document.querySelector('.gallery__next');
